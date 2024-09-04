5e1bbd0e848062e61c045d137845e5474d6eea8f
Luiz Augusto Wendt <luiz.wendt@outlook.com>
2024-09-04 09:21:24 -0300

    and now?

--- BuildBinaries.bat
+++ BuildBinaries.bat
@@ -1,8 +1,8 @@
 @REM try out graph?
 @REM call "D:/UE_5.4/Engine/Binaries/DotNET/UnrealBuildTool/UnrealBuildTool.exe" -projectfiles -project="%~dp0\BuildingBinaries1.uproject" -game -engine -progress
 
-set ENGINE_PATH="D:\UE_5.4"
-set PROJECT_NAME="BuildingBinaries1"
+set ENGINE_PATH=D:/UE_5.4
+set PROJECT_NAME=BuildingBinaries1
 set DIRECTORY=%~dp0
 
-call "%ENGINE_PATH%\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -platform=Win64 -clientconfig=Development -project="%DIRECTORY%%PROJECT_NAME%.uproject" -build %*
+call "%ENGINE_PATH%/Engine/Build/BatchFiles/RunUAT.bat" BuildCookRun -platform=Win64 -clientconfig=Development -project="%DIRECTORY%%PROJECT_NAME%.uproject" -build %*
